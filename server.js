cac44b205a69d2412aeed767079335a60f5d7760
John Aeron Aragones <jaaragones@myseneca.ca>
2022-09-08 23:19:26 -0400

    Assignment 1 changes

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: John Aeron Aragones    Student ID: 121107213    Date: September 8, 2022
+*
+*  Online (Cyclic) URL:  https://sore-lime-chipmunk-fez.cyclic.app/
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("John Aeron Aragones - 121107213");
 });
 
 // setup http server to listen on HTTP_PORT
